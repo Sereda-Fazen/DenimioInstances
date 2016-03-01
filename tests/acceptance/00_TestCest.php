bd5b5466b98e4032b972f3f6d77ba1ee7a5ec7c6
Sereda-Fazen <fazen7@mail.ru>
2016-03-01 15:37:17 +0200

    8

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -5,6 +5,12 @@ class TestCest {
 
 
 
+    function loginWrongEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('test@test.com', '123456');
+        $I->see('Invalid login or password.', 'li.error-msg');
+        $I->comment('Expected result: Please enter a valid email address.');
+    }
+
     function forgotSuccess(Step\Acceptance\ForgotPassSteps $I, \Page\ForgotPass $forgotPage)
     {
         $forgotPage->forgot('denimio_test@yahoo.com');
