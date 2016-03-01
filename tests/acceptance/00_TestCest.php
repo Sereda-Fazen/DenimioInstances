a15995fb5da4ef3cf7feaceac1a4edf45b2e26ac
Sereda-Fazen <fazen7@mail.ru>
2016-03-01 11:08:44 +0200

    5

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -5,7 +5,23 @@ class TestCest {
 
 
 
+    function forgotSuccess(Step\Acceptance\ForgotPassSteps $I, \Page\ForgotPass $forgotPage)
+    {
+        $forgotPage->forgot('denimio_test@yahoo.com');
+    }
+    //
+    function enterNewPass (Step\Acceptance\ForgotPassSteps $I)
+    {
+        $I->gMailAuth();
+        $I->remoteWindow();
+        $I->newPass();
+    }
 
+
+    function deleteOldMsg(Step\Acceptance\LoginSteps $I, Page\ForgotPass $deleteMsg){
+        $deleteMsg->deleteMsg();
+
+    }
 
 
 
