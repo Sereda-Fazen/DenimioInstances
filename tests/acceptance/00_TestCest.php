0a22bfba92c91b4ec6659f689229f3bd94081c9b
Sereda-Fazen <fazen7@mail.ru>
2016-03-01 10:39:33 +0200

    3

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -5,12 +5,6 @@ class TestCest {
 
 
 
-    function checkCheckoutForGuestWithAddingDifferentAddress (Step\Acceptance\CheckoutSteps $I, \Page\Checkout $guestPage)
-    {
-        $I->checkoutForGuestWithAddingDifferentAddress();
-
-    }
-
 
 
 
